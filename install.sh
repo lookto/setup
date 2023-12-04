049feaeb1dc5b43ddb674ec2a7a0d5794d537551
lookto <git@lookto.de>
2023-12-04 06:42:38 +0100

    feat: add vsc

--- install.sh
+++ install.sh
@@ -10,15 +10,21 @@ fi
 
 # Commands for each system
 arch_commands=(
-	"sudo pacman -S neovim"
+	"pacman -S neovim"
+	"pacman -S  visual-studio-code-bin"
 )
 
 ubuntu_commands=(
-	"git clone https://github.com/neovim/neovim"
-	"apt install cmake"
-	"cd neovim && git checkout stable && make CMAKE_BUILD_TYPE=RelWithDebInfo"
-	"cd build && cpack -G DEB && sudo dpkg -i nvim-linux64.deb"
-	"cd ${PWD} && rm -rf neovim"
+	# "apt install software-properties-common apt-transport-https wget -y"
+	# "git clone https://github.com/neovim/neovim"
+	# "apt install cmake"
+	# "cd neovim && git checkout stable && make CMAKE_BUILD_TYPE=RelWithDebInfo"
+	# "cd build && cpack -G DEB && sudo dpkg -i nvim-linux64.deb"
+	# "cd ${PWD} && rm -rf neovim"
+	# "wget -q https://packages.microsoft.com/keys/microsoft.asc -O- | sudo apt-key add -"
+	# "add-apt-repository \"deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main\" -y"
+	# "apt update"
+	# "apt install code -y"
 )
 
 # Commands to be run on every system
@@ -27,6 +33,15 @@ all_commands=(
 	"mkdir -p ${USER_HOME}/.config"
 	"mv ${USER_HOME}/.config/nvim{,.bak}"
 	"ln -s ${PWD}/configs/nvim ${USER_HOME}/.config"
+	"echo 'Symlinking vsc configs'"
+	"mkdir -p ${USER_HOME}/.config/Code/User"
+	"rm ${USER_HOME}/.config/Code/User/settings.json"
+	"ln -s ${PWD}/configs/vsc/settings.json ${USER_HOME}/.config/Code/User/settings.json"
+	"rm ${USER_HOME}/.config/Code/User/keybindings.json"
+	"ln -s ${PWD}/configs/vsc/keybindings.json ${USER_HOME}/.config/Code/User/keybindings.json"
+	"mkdir -p ${USER_HOME}/.vscode/extensions"
+	"rm ${USER_HOME}/.vscode/extensions/extensions.json"
+	"ln -s ${PWD}/configs/vsc/extensions.json ${USER_HOME}/.vscode/extensions/extensions.json"
 )
 
 # Function to exectute array of Commands
